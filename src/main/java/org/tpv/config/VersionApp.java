0469ed476b52dc88031664124e86fb7d8bc88ca5
María Ahmed Naz <man00017@red.ujaen.es>
2026-07-25 21:04:42 +0200

    Primera version del programa lista para desplegar

--- src/main/java/org/tpv/config/VersionApp.java
+++ src/main/java/org/tpv/config/VersionApp.java
@@ -3,5 +3,5 @@ package org.tpv.config;
 public class VersionApp {
 
     public static final String VERSION_ACTUAL = "1.0.0";
-    public static final String URL_CHECK_VERSION = "https://tuservidor.com/tpv/version.json";
+    public static final String URL_CHECK_VERSION = "https://raw.githubusercontent.com/maria5557/software-venta/refs/heads/main/version.json";
 }
